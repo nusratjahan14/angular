652b832069f7226b22bf8da17342acdc07d517a3
nusrat jahan <n.j.naima222@gmail.com>
2024-08-17 00:48:29 +0600

    add

--- myangular/src/app/app-routing.module.ts
+++ myangular/src/app/app-routing.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EmployeeComponent } from './employee/employee.component';
 import { LocationComponent } from './location/location.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {path: 'employee', component:EmployeeComponent},
-  {path: 'location', component:LocationComponent}
+  {path: 'location', component:LocationComponent},
+  {
+    path: 'login', component: LoginComponent
+  },
+  // {
+  //   path: '', redirectTo: 'login', pathMatch: 'full'
+  // }
 ];
 
 @NgModule({
--- myangular/src/app/app.module.ts
+++ myangular/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployeeComponent } from './employee/employee.component';
 import { LocationComponent } from './location/location.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EmployeeComponent,
-    LocationComponent
+    LocationComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
