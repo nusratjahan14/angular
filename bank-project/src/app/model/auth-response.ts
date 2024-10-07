4f1bf16040e214d5d0efcbeed8c26076574f6837
nusrat jahan <n.j.naima222@gmail.com>
2024-10-07 23:36:07 +0600

    add

---
+++ bank-project/src/app/model/auth-response.ts
@@ -0,0 +1,10 @@
+import { UserModel } from "./user.model";
+
+export interface AuthResponse {
+
+
+    token: string;
+    user: UserModel;
+
+
+}
---
+++ bank-project/src/app/model/customer.model.ts
@@ -0,0 +1,17 @@
+export class Customer {
+   
+    aid ?: number;
+    accountNumber ?: string;
+    firstName ?: string;
+    lastName ?: string;
+    gender ?: string;
+    email ?: string;
+    address ?: string;
+    cell ?: string;
+    nid ?: string;
+    accountType ?: string;
+    currentBalence ?: number;
+    creatingDate ?: Date;
+    image ?: string;
+    status ?: boolean;
+  }
